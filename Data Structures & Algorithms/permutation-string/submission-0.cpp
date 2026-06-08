367b828b722fc2d8691287c15f700aab5965ced1
mkv4540 <mkv4540@gmail.com>
2026-06-08 10:36:23 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int left = 0;
+        vector<int>char_set(26, 0), target_set(26, 0);
+        for (char c : s1){
+            char_set[c-'a']++;
+        }
+        for (char c : s2){
+            target_set[c-'a']++;
+            while (left < s2.size() && target_set[c-'a'] > char_set[c-'a']){
+                target_set[s2[left]-'a']--;
+                left++;
+            }
+            if (char_set == target_set){
+                return true;
+            }
+        }
+        return false;
+    }
+};
