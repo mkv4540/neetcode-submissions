d24bd5b13fb094a22d748e7f0990b8d2b158bbfb
mkv4540 <mkv4540@gmail.com>
2026-06-09 11:18:51 +0530

    Add: merge-intervals - submission-4

---
+++ Data Structures & Algorithms/merge-intervals/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>res;
+        sort (intervals.begin(), intervals.end());
+        int start = intervals[0][0], end = intervals[0][1];
+        for (int index = 1; index < intervals.size(); index++){
+            if (end >= intervals[index][0]){
+                cout<<start<<endl;
+                start = min(start, intervals[index][0]);
+                end = max(end, intervals[index][1]);
+            }else{
+                res.push_back({start, end});
+                start = intervals[index][0];
+                end = intervals[index][1];
+            }
+        }
+        res.push_back({start, end});
+        return res;
+    }
+};
