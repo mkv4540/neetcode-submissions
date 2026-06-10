65c5001668b5090b962bb055f6ed485dce41ec72
mkv4540 <mkv4540@gmail.com>
2026-06-10 09:48:50 +0530

    Add: rotate-array - submission-4

---
+++ Data Structures & Algorithms/rotate-array/submission-4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+       k = k % nums.size();
+       reverse (nums.end() - k, nums.end()); // a, b'
+       reverse (nums.begin(), nums.end() - k); // a', b'
+       reverse(nums.begin(), nums.end());
+    }
+};
+
+// 1 2 3 4 5 6 7
+// 1 2 3 4 5 7 6
+// 5 4 3 2 1 7 6
+// 6 7 1 2 3 4 5
+// 6 7 1 2 3 4 5
