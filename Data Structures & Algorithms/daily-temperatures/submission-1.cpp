7ffe2c60f14867a4384d32b6e91868717d6dd4f0
mkv4540 <mkv4540@gmail.com>
2026-06-10 10:50:06 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<pair<int, int>>st;
+        vector<int>ans(temperatures.size(), 0);
+        for (int index = 0; index < temperatures.size(); index++){
+            while (st.size() && st.top().first < temperatures[index]){
+                ans[st.top().second] = index - st.top().second;
+                st.pop();
+            }
+            st.push({temperatures[index], index});
+        }
+        while (st.size()){
+            ans[st.top().second] = 0;
+            st.pop();
+        }
+        return ans; 
+    }
+};
